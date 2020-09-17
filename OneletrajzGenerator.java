92d9cfc71d35ea458a14e606ec255a7d13673957
MarcusCalgar <paraszthybalazs@gmail.com>
2020-09-17 15:32:56 +0200

    Added second line

--- OneletrajzGenerator.java
+++ OneletrajzGenerator.java
@@ -3,5 +3,6 @@ package oneletrajzgenerator;
 public class OneletrajzGenerator {    
     public static void main(String[] args) {
         System.out.println("Név: Paraszthy Balázs Vilmos");
+        System.out.println("Email: paraszthybalazs@gmail.com");
     }
 }
