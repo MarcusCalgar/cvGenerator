6c6cacd619013da02aa40813f8e42a351fe22562
MarcusCalgar <paraszthybalazs@gmail.com>
2020-09-17 11:11:50 +0200

    Initial commit

---
+++ OneletrajzGenerator.java
@@ -0,0 +1,7 @@
+package oneletrajzgenerator;
+
+public class OneletrajzGenerator {    
+    public static void main(String[] args) {
+        System.out.println("Név: Paraszthy Balázs Vilmos");
+    }
+}
