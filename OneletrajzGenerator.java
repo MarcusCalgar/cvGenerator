713f887115307464af1e16ca0ee957fb11207a02
MarcusCalgar <45832404+MarcusCalgar@users.noreply.github.com>
2020-09-21 13:44:18 +0200

    Delete OneletrajzGenerator.java
    File renamed, not needed anymore

--- OneletrajzGenerator.java
+++
@@ -1,8 +0,0 @@
-package oneletrajzgenerator;
-
-public class OneletrajzGenerator {    
-    public static void main(String[] args) {
-        System.out.println("Név: Paraszthy Balázs Vilmos");
-        System.out.println("Email: paraszthybalazs@gmail.com");
-    }
-}
