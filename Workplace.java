10540e240d3ddac560f42fb0c7aded2981f353b1
MarcusCalgar <paraszthybalazs@gmail.com>
2020-09-21 13:06:18 +0200

    Added Person.java and Workplace.java

---
+++ Workplace.java
@@ -0,0 +1,5 @@
+package cvGenerator;
+
+public class Workplace {
+
+}
---
+++ cvGenerator.java
@@ -0,0 +1,8 @@
+package cvGenerator;
+
+public class cvGenerator {    
+    public static void main(String[] args) {
+        System.out.println("Név: Paraszthy Balázs Vilmos");
+        System.out.println("Email: paraszthybalazs@gmail.com");
+    }
+}
