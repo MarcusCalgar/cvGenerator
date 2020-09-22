1dbb090499245f0ce3c0df1e963e85577f69e987
MarcusCalgar <paraszthybalazs@gmail.com>
2020-09-22 20:56:15 +0200

    jobTitle field added, also getter/setter and constructor modified

--- Workplace.java
+++ Workplace.java
@@ -1,15 +1,17 @@
 package cvGenerator;
 
 public class Workplace {
- private int startYear;
+    private int startYear;
     private int endYear;
     private String companyName;
+    private String jobTitle;
     private String jobDescription;
 
-    public Workplace(int startYear, int endYear, String companyName, String jobDescription) {
+    public Workplace(int startYear, int endYear, String companyName, String jobTitle, String jobDescription) {
         this.startYear = startYear;
         this.endYear = endYear;
         this.companyName = companyName;
+        this.jobTitle = jobTitle;
         this.jobDescription = jobDescription;
     }
 
@@ -43,5 +45,13 @@ public class Workplace {
 
     public void setJobDescription(String jobDescription) {
         this.jobDescription = jobDescription;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
     }
 }
